ae4e4b04254741ddafdd33129220ded5d1082261
cesardaniel92 <ctamayoc@asu.edu>
2019-09-25 21:41:23 -0700

    SNS notification test added to write_data lambda function

--- AWS/shs-apex/functions/writeData/index.js
+++ AWS/shs-apex/functions/writeData/index.js
@@ -25,6 +25,30 @@ exports.handle = function(e, ctx, cb) {
         TableName: 'SHS-SensorData'
     };
 
+    // SNS Notification test:
+    // Set region
+    AWS.config.update({region: 'us-west-1'});
+
+    // Create publish parameters
+    var message_params = {
+      Message: 'This is a test from Lambda function shs_writeData. Thanks!', /* required */
+      TopicArn: 'arn:aws:sns:us-west-1:136159130349:dynamodb'
+    };
+
+    // Create promise and SNS service object
+    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(message_params).promise();
+
+    // Handle promise's fulfilled/rejected states
+    publishTextPromise.then(
+      function(data) {
+        console.log("Message ${message_params.Message} send sent to the topic ${message_params.TopicArn}");
+        console.log("MessageID is " + data.MessageId);
+      }).catch(
+        function(err) {
+        console.error(err, err.stack);
+      });
+
+
     docClient.put(params, function(err, data){
         if(err){
             cb(err, null);
